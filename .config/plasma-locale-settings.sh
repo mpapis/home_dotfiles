e4a4efbbdcc90aabaca8309f9530a1262dd58371
Michal Papis <mpapis@gmail.com>
2017-07-23 23:49:22 +0200

    Add locale settings for KDE

---
+++ .config/plasma-locale-settings.sh
@@ -0,0 +1,7 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_US.UTF-8
+export LC_TIME=pl_PL.UTF-8
+export LC_MEASUREMENT=pl_PL.UTF-8
